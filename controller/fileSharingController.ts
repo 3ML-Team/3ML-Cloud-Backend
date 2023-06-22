0102c098f214bfae85e526a672ea0c1fde3909c1
MFB3000 <max.bernasch@gmail.com>
2023-06-22 11:37:54 +0200

    Added handling for single file download

--- controller/fileSharingController.ts
+++ controller/fileSharingController.ts
@@ -1,17 +1,20 @@
 import { Request, Response } from "express";
-import * as fs from 'fs';
-import archiver from 'archiver';
+import * as fs from "fs";
+import archiver from "archiver";
 import FileModel from "../model/file-model";
 import { UserPayload } from "../interfaces/UserPayload";
 import DownloadLinkModel from "../model/downloadlink-model";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 export const test = (req: Request, res: Response): void => {
   console.log(req.user);
   res.sendStatus(200);
 };
 
-export const uploadFiles = async (req: Request, res: Response): Promise<void> => {
+export const uploadFiles = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   try {
     const user = req.user as UserPayload;
     const files = req.files as Express.Multer.File[];
@@ -32,8 +35,11 @@ export const uploadFiles = async (req: Request, res: Response): Promise<void> =>
       })
     );
 
-    const fileIds = uploadedFiles.map(file => file.id);
-    const downloadLink = new DownloadLinkModel({ linkId: uuidv4(), files: fileIds });
+    const fileIds = uploadedFiles.map((file) => file.id);
+    const downloadLink = new DownloadLinkModel({
+      linkId: uuidv4(),
+      files: fileIds,
+    });
     await downloadLink.save();
 
     const currentHost = req.headers.host;
@@ -46,37 +52,46 @@ export const uploadFiles = async (req: Request, res: Response): Promise<void> =>
   }
 };
 
-export const downloadAll = async (req: Request, res: Response): Promise<void> => {
+export const downloadAll = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   try {
     const linkId = req.params.linkId;
     console.log("linkId in downloadAll ist: " + linkId);
     const downloadLink = await DownloadLinkModel.findOne({ linkId: linkId });
 
     if (!downloadLink) {
-      res.status(404).json({ error: 'Download link not found' });
+      res.status(404).json({ error: "Download link not found" });
       return;
     }
 
-    const archive = archiver('zip', {
-      zlib: { level: 9 } // Sets the compression level.
+    const files = await FileModel.find({ _id: { $in: downloadLink.files } });
+    // if there's only one file, download it directly
+    if (files.length === 1) {
+      const file = files[0];
+      return res.download(file.path, file.originalName);
+    }
+
+    
+    const archive = archiver("zip", {
+      zlib: { level: 9 },
     });
 
-    archive.on('error', function(err: Error) {
+    archive.on("error", function (err: Error) {
       res.status(500).send({ error: err.message });
     });
 
-    res.setHeader('Content-Type', 'application/zip');
-    res.setHeader('Content-Disposition', `attachment; filename="download.zip"`);
+    res.setHeader("Content-Type", "application/zip");
+    res.setHeader("Content-Disposition", `attachment; filename="download.zip"`);
 
     archive.pipe(res);
 
-    const files = await FileModel.find({ '_id': { $in: downloadLink.files } });
-
     for (const file of files) {
       const fileStream = fs.createReadStream(file.path);
-      fileStream.on('error', (err: Error) => {
+      fileStream.on("error", (err: Error) => {
         console.error(`Error in reading file: ${err}`);
-        return res.status(500).send({ error: 'Error in reading file' });
+        return res.status(500).send({ error: "Error in reading file" });
       });
       archive.append(fileStream, { name: file.originalName });
     }
@@ -84,12 +99,12 @@ export const downloadAll = async (req: Request, res: Response): Promise<void> =>
     archive.finalize();
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: "Server error" });
   }
 };
 
 export default {
   test,
   uploadFiles,
-  downloadAll
+  downloadAll,
 };
